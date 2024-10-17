f2be5d4849a3e46c1fe00a1e3ce87936680f2823
Shohanur-Shohan <shohanurshohan734@gmail.com>
2024-10-17 06:29:45 +0600

    imported pacakages

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,6 +1,13 @@
+import User from "../models/UserSchema.js"
+import Doctors from "../models/DoctorSchema.js"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+
 export const register = async (req, res)=>{
     try {
         console.log("clicked register")
+        const { name, email, password, photo, role, gender } = req.body;
+
     } catch (error) {
         console.log("Error at authController", error)
     }
